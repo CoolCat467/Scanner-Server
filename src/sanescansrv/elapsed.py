73cd1774bef8b638964ac085e1703e752188947f
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-05-17 00:00:33 -0500

    Add license header info

--- src/sanescansrv/elapsed.py
+++ src/sanescansrv/elapsed.py
@@ -1,6 +1,32 @@
-"""Get elapsed time."""
+"""Elapsed Time."""
 
-from collections.abc import Iterable
+# Programmed by CoolCat467
+
+from __future__ import annotations
+
+# Elapsed Time
+# Copyright (C) 2022-2024  CoolCat467
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+__title__ = "Elapsed Time"
+__author__ = "CoolCat467"
+__license__ = "GNU General Public License Version 3"
 
 
 def split_time(seconds: int) -> list[int]:
